adf3cef9e0bb5a0a3cedb1d04aefd9473a544153
TimurGar <timur141633@gmail.com>
2024-09-01 01:18:42 -0400

    sidebar button changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -41,10 +41,10 @@ export const Sidebar = () => {
         )}
       </AnimatePresence>
       <button
-        className="fixed lg:hidden bottom-4 right-4 h-8 w-8 border border-neutral-200 rounded-full backdrop-blur-sm flex items-center justify-center"
+        className="fixed lg:hidden bottom-4 right-4 h-16 w-16 border border-neutral-200 rounded-full backdrop-blur-sm flex items-center justify-center"
         onClick={() => setOpen(!open)}
       >
-        <IconLayoutSidebarRightCollapse className="h-4 w-4 text-secondary" />
+        <IconLayoutSidebarRightCollapse className="h-6 w-6 text-secondary" />
       </button>
     </>
   );
