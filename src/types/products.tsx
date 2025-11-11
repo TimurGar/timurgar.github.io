914c06b263fff6ab9a8275c7581699e564fff7b2
TimurGar <timur141633@gmail.com>
2025-11-10 23:25:28 -0500

    Update Product type to allow React nodes in description

--- src/types/products.tsx
+++ src/types/products.tsx
@@ -1,7 +1,7 @@
 export type Product = {
   robotics: boolean | undefined;
   title: string;
-  description: string;
+  description: string | React.ReactNode;
   thumbnail: string;
   images: string[];
   href: string;
